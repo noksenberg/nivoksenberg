190844bcacab3868464f07ace404d2c3c31dd83e
Niv Oksenberg <nivoksenberg@gmail.com>
2020-04-17 00:19:18 -0400

    Remove deprecated analytics script

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,9 +20,14 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: "UA-163871634-1X"
+        // your google analytics tracking id
+        trackingId: `UA-163871634-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true
       }
     }
   ]
